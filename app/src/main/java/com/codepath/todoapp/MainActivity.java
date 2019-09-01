8c08db8055abbfd7041725b512212f92723287c8
joyceannie <joyceannie111@gmail.com>
2019-08-31 19:51:20 -0700

    Implemented persistence

--- app/src/main/java/com/codepath/todoapp/MainActivity.java
+++ app/src/main/java/com/codepath/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.codepath.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,6 +11,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,15 +29,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        items = new ArrayList<String>();
+        readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems = (ListView) findViewById(R.id.lvItems);
         lvItems.setAdapter(itemsAdapter);
 
         //mock data
-        items.add("First Item");
-        items.add("Second Item");
-        items.add("Third Item");
+        //items.add("First Item");
+        //items.add("Second Item");
+        //items.add("Third Item");
 
         setupListViewListener();
     }
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
+        writeItems();
         Toast.makeText(getApplicationContext(), "Item added to the list", Toast.LENGTH_SHORT).show();
     }
 
@@ -50,9 +57,31 @@ public class MainActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 items.remove(i);
                 itemsAdapter.notifyDataSetChanged();
+                writeItems();
                 return true;
             }
         });
     }
 
+    private  File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch(IOException e){
+            Log.e("MainActivity", "Error reading file", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch(IOException e) {
+            Log.e("MainActivity", "error writing file", e);
+        }
+
+    }
 }
