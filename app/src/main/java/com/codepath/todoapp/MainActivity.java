aefbc5fb457c5319c08e2a6c760bc9db2eafcd3d
joyceannie <joyceannie111@gmail.com>
2019-08-31 18:30:04 -0700

    Implemented add/remove functionalities

--- app/src/main/java/com/codepath/todoapp/MainActivity.java
+++ app/src/main/java/com/codepath/todoapp/MainActivity.java
@@ -3,12 +3,56 @@ package com.codepath.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(itemsAdapter);
+
+        //mock data
+        items.add("First Item");
+        items.add("Second Item");
+        items.add("Third Item");
+
+        setupListViewListener();
     }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        Toast.makeText(getApplicationContext(), "Item added to the list", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
 }
